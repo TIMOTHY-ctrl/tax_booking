b43830c7019c57b820c830467029093391e31cf6
TIMOTHY-ctrl <mwizerwatimothy204@gmail.com>
2025-10-15 01:05:39 +0300

    set pu for the project

---
+++ tax_booking/src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-2xl font-bold">Login Page</h1>
+    </div>
+  );
+}
---
+++ tax_booking/src/app/welcome/page.tsx
@@ -0,0 +1,3 @@
+export default function WelcomePage() {
+  return <h1>Welcome to my Next.js app!</h1>;
+}
